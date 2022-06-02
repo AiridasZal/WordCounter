c1deab8cc46af52cc38b0362e27090c19c891fdf
AiridasZal <air.airidas1@gmail.com>
2022-06-02 14:16:24 +0300

    Added file validation

--- Counter.cpp
+++ Counter.cpp
@@ -8,13 +8,38 @@
 #include <sstream>
 #include <algorithm>
 
-void counter(std::string fname, std::map<std::string, std::vector<int>> &word_map, std::vector<std::string> &url) {
+void file_validation(std::string &file_name){
+    std::cout << "Enter the file name using the following format: [example.txt]" << std::endl;
+    std::cin >> file_name;
+    if(file_name.find(".txt") == std::string::npos){
+        std::cout << "File format is not correct. Please try again." << std::endl;
+        file_validation(file_name);
+    }
+    if(file_name.find(" ") != std::string::npos){
+        std::cout << "File name cannot contain spaces. Please try again." << std::endl;
+        file_validation(file_name);
+    }
+    std::ifstream in(file_name);
+    if(!in){
+        std::cout << "File does not exist. Please try again." << std::endl;
+        file_validation(file_name);
+    }
+    std::string line;
+    std::getline(in, line);
+    if(line.empty()){
+        std::cout << "File is empty. Please try again." << std::endl;
+        file_validation(file_name);
+    }
+    in.close();
+}
+
+void counter(std::string file_name, std::map<std::string, std::vector<int>> &word_map, std::vector<std::string> &url) {
     std::istringstream iss;
     std::string line;
     std::string word;
     int line_num = 0;
 
-    std::ifstream in(fname);
+    std::ifstream in(file_name);
     if (!in) {
         throw std::runtime_error("Could not open file");
     }
@@ -49,6 +74,7 @@ void counter(std::string fname, std::map<std::string, std::vector<int>> &word_ma
         }
     }
     in.close();
+    std::cout << "File successfully read." << std::endl;
 }
 void output(std::string outfile, std::map<std::string, std::vector<int>> word_map, std::vector<std::string> url)
 {
@@ -83,18 +109,18 @@ void output(std::string outfile, std::map<std::string, std::vector<int>> word_ma
         out << url[i] << std::endl;
     }
     out.close();
-
+    std::cout << "Results have been outputted to: " << outfile << std::endl;
 }
 
 int main()
 {
-    std::string fname="example.txt";
+    std::string file_name;
     std::string outfile="Rezultatai.txt";
     
     std::map<std::string, std::vector<int>> word_map;
     std::vector<std::string> url;
-
-    counter(fname, word_map, url);
+    file_validation(file_name);
+    counter(file_name, word_map, url);
     output(outfile, word_map, url);
     
     
